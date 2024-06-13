9e309c8e2a24103742e4bfaf015b5a2533bb7996
juancclcarvalho <juancclcarvalho@gmail.com>
2024-06-13 10:08:33 -0400

    adding puzzle aux task

--- loader.py
+++ loader.py
@@ -9,6 +9,9 @@ import numpy as np
 import random
 import cv2
 
+from torchvision.transforms.functional import resize
+
+
 from config import Config
 config = Config()
 
@@ -47,6 +50,45 @@ class RotationLoader(Dataset):
             rotations = [0,1,2,3]
             random.shuffle(rotations)
             return imgs[rotations[0]], imgs[rotations[1]], imgs[rotations[2]], imgs[rotations[3]], rotations[0], rotations[1], rotations[2], rotations[3], self.img_path[idx]
+        
+class PuzzleLoader(Dataset):
+    def __init__(self, is_train=True, transform=None, path=config.data_dir):
+        self.is_train = is_train
+        self.transform = transform
+        # self.h_flip = transforms.RandomHorizontalFlip(p=1)
+        if self.is_train == 0: # train
+            self.img_path = glob.glob(f'{path}/train/*/*')
+        else:
+            self.img_path = glob.glob(f'{path}/train/*/*')
+
+    def __len__(self):
+        return len(self.img_path)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.img_path[idx])
+        img = Image.fromarray(img)
+
+        if self.is_train:
+            img = self.transform(img)
+            img1 = resize(img[:, :16, :16], (32, 32))
+            img2 = resize(img[:, :16, 16:], (32, 32))
+            img3 = resize(img[:, 16:, :16], (32, 32))
+            img4 = resize(img[:, 16:, 16:], (32, 32))
+            imgs = [img1, img2, img3, img4]
+            permutations = [[0,1,2,3], [0,1,3,2], [0,2,1,3], [0,2,3,1], [0,3,1,2], [0,3,2,1], [1,0,2,3], [1,0,3,2], [1,2,0,3], [1,2,3,0], [1,3,0,2], [1,3,2,0], [2,0,1,3], [2,0,3,1], [2,1,0,3], [2,1,3,0], [2,3,0,1], [2,3,1,0], [3,0,1,2], [3,0,2,1], [3,1,0,2], [3,1,2,0], [3,2,0,1], [3,2,1,0]]
+            random.shuffle(permutations)
+            return imgs[permutations[0][0]], imgs[permutations[0][1]], imgs[permutations[0][2]], imgs[permutations[0][3]], permutations[0][0], permutations[0][1], permutations[0][2], permutations[0][3]
+        
+        else:
+            img = self.transform(img)
+            img1 = resize(img[:, :16, :16], (32, 32))
+            img2 = resize(img[:, :16, 16:], (32, 32))
+            img3 = resize(img[:, 16:, :16], (32, 32))
+            img4 = resize(img[:, 16:, 16:], (32, 32))
+            imgs = [img1, img2, img3, img4]
+            permutations = [[0,1,2,3], [0,1,3,2], [0,2,1,3], [0,2,3,1], [0,3,1,2], [0,3,2,1], [1,0,2,3], [1,0,3,2], [1,2,0,3], [1,2,3,0], [1,3,0,2], [1,3,2,0], [2,0,1,3], [2,0,3,1], [2,1,0,3], [2,1,3,0], [2,3,0,1], [2,3,1,0], [3,0,1,2], [3,0,2,1], [3,1,0,2], [3,1,2,0], [3,2,0,1], [3,2,1,0]]
+            random.shuffle(permutations)
+            return imgs[permutations[0][0]], imgs[permutations[0][1]], imgs[permutations[0][2]], imgs[permutations[0][3]], permutations[0][0], permutations[0][1], permutations[0][2], permutations[0][3], self.img_path[idx]
 
 class Loader2(Dataset):
     def __init__(self, is_train=True, transform=None, path=config.data_dir, path_list=None):
